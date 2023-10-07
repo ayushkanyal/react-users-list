fd9d00fe94396898b2e2a62720f3fa53cbc788c9
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-07 20:00:50 +0530

    Update App.js
    Added custom title,text props for invalid age and usernames inn Error module

--- src/App.js
+++ src/App.js
@@ -1,83 +1,54 @@
-import logo from "./assets/images/logo.svg";
+import { useState } from "react";
+import List from "./components/Output/List";
+import Form from "./components/Input/Form";
+import ErrorModal from "./components/Output/ErrorModal";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [error, setError] = useState(false);
+  const [inputs, setInputs] = useState([]);
+
+  function handleClick() {
+    setError(false);
+  }
+
+  function fetchSubmit(input) {
+    console.log(input.username.length);
+    console.log(input.age.length);
+    if (input.username.length === 0) {
+      setError({
+        title: "Invalid Username",
+        message: "Please Enter a valid Name.",
+      });
+      return;
+    }
+    if (input.age <= 0) {
+      setError({
+        title: "Invalid Age",
+        message: "Please Enter a valid Age(>0).",
+      });
+      return;
+    } else {
+      setError(true);
+      setName((values) => [...values, input.username]);
+      setAge((values) => [...values, input.age]);
+      setInputs((values) => [...values, input]);
+    }
+  }
+
   return (
-    <div className="App">
-      <main>
-        <nav>
-          <img src={logo} alt="Website logo"></img>
-          <ul>
-            <li>Home</li>
-            <li>New</li>
-            <li>Popular</li>
-            <li>Trending</li>
-            <li>Categories</li>
-          </ul>
-        </nav>
-        <section className="title-cover">
-          <h1>The Bright Future of Web 3.0?</h1>
-          <p>
-            We dive into the next evolution of the web that claims to put the
-            power of the platforms back into the hands of the people. But is it
-            really fulfilling its promise?{" "}
-          </p>
-          <button>Read more</button>
-        </section>
-        <section className="new-arrivals">
-          <h2>New</h2>
-          <article>
-            <h3>Hydrogen VS Electric Cars</h3>{" "}
-            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-          </article>
-          <article>
-            <h3>The Downsides of AI Artistry</h3>
-            <p>
-              What are the possible adverse effects of on-demand AI image
-              generation?
-            </p>
-          </article>
-          <article>
-            <h3>Is VC Funding Drying Up?</h3>
-            <p>
-              Private funding by VC firms is down 50% YOY. We take a look at
-              what that means.
-            </p>
-          </article>
-        </section>
-        <section className="popular-articles">
-          <article>
-            <img></img>
-            <h3>01</h3>
-            <h4>Reviving Retro PCs</h4>
-            <p>What happens when old PCs are given modern upgrades?</p>
-          </article>
-          <article>
-            <img></img>
-            <h3>02</h3>
-            <h4>Top 10 Laptops of 2022</h4>
-            <p>Our best picks for various needs and budgets.</p>
-          </article>
-          <article>
-            <img></img>
-            <h3>03</h3>
-            <h4>The Growth of Gaming</h4>
-            <p>How the pandemic has sparked fresh opportunities.</p>
-          </article>
-        </section>
-        <footer>
-          <div class="attribution">
-            Challenge by{" "}
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by <a href="#">Your Name Here</a>.
-          </div>
-        </footer>
-      </main>
+    <div>
+      <Form onAdd={fetchSubmit} />
+      <List list={inputs} />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          text={error.message}
+          onClick={handleClick}
+        />
+      )}
     </div>
   );
 }
