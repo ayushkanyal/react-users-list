88d00470df9aa089ba5c2390ce3eba7e11a55e52
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-08 01:06:33 +0530

    Update ErrorModal.js
    Added overlay element html to display along with error message. Changesd Wrapper to React.fragmenter

--- src/components/Output/ErrorModal.js
+++ src/components/Output/ErrorModal.js
@@ -7,18 +7,19 @@ export default function ErrorModal(props){
     }
 
     return(
-        <div className={style.error}>
-            <header>
-                <h2>{props.title}</h2>
-            </header>
-            <div>
-                <p>{props.text}</p>
-            </div>
-            <footer>
-                <button onClick={handleClick}>
-                    Okay
-                </button>
-            </footer>
+       <>
+      <div className={style.overlay}></div>
+      <div className={style.error}>
+        <header>
+          <h2>{props.title}</h2>
+        </header>
+        <div>
+          <p>{props.text}</p>
         </div>
+        <footer>
+          <button onClick={handleClick}>Okay</button>
+        </footer>
+      </div>
+    </>
     )
 }
