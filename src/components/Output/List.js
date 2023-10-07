2eaef9def5b6af3bf8cfbdd1ed8dde3339560c63
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-07 19:00:57 +0530

    Update List.js
    Added list item feature which shows entered results.

--- src/components/Output/List.js
+++ src/components/Output/List.js
@@ -1,12 +1,24 @@
 import style from "./List.module.css";
 
-export default function List(props){
-    console.log(props.list);
-    const recieved = props.list;
-    const itemsList = recieved.map((entry) => (<li><span className={style.border}>{entry.username} ({entry.age} years old)</span></li>));
-    return (
-        <ul className={style.ul}>
-            {itemsList}
-        </ul>
-    )
+export default function List(props) {
+    console.log("List was activated");
+  console.log(props.list);
+  const recieved = props.list;
+ 
+  return (
+    <ul className={style.ul}>
+      {recieved.length === 0 ? (
+        ""
+      ) : (
+        recieved.map((entry) => (
+          <li>
+            <span className={style.border}>
+              {entry.username} ({entry.age} years old)
+            </span>
+          </li>
+        ))
+      )}
+      ;
+    </ul>
+  );
 }
