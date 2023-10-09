b922c58f06cdae981090d4773ea365004268cd49
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-09 20:08:52 +0530

    Update ErrorModal.js
    Uses React.fragment() and ReactDOM create portal function to simplify the DOM tree for more efficient rendering

--- src/components/Output/ErrorModal.js
+++ src/components/Output/ErrorModal.js
@@ -1,25 +1,37 @@
 import style from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
 
-export default function ErrorModal(props){
+const Backdrop = () => {
+  return <div className={style.overlay}></div>;
+};
 
-    function handleClick(){
-        props.onClick();
-    }
-
-    return(
-       <>
-      <div className={style.overlay}></div>
-      <div className={style.error}>
-        <header>
-          <h2>{props.title}</h2>
-        </header>
-        <div>
-          <p>{props.text}</p>
-        </div>
-        <footer>
-          <button onClick={handleClick}>Okay</button>
-        </footer>
+const Modal = (props) => {
+  return (
+    <div className={style.error}>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.text}</p>
       </div>
+      <footer>
+        <button onClick={props.onClick}>Okay</button>
+      </footer>
+    </div>
+  );
+};
+
+export default function ErrorModal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Modal title={props.title} text={props.text} onClick={props.onClick} />,
+        document.getElementById("modal-root")
+      )}
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("overlay-root")
+      )}
     </>
-    )
+  );
 }
